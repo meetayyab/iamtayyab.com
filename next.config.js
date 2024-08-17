f14f7a5236805fce010684d8d7f512cacebbe683
Muhammad Tayyab <96627100+muhammadtayyab3411@users.noreply.github.com>
2024-08-18 03:52:16 +0500

    set the stale time for dynamic routes to zero

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const { default: dynamic } = require('next/dynamic')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+    }
+  }
+}
 
 module.exports = nextConfig
