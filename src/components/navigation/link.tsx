a9f3dde1f33570bb16f8e4ad73d3d8c5f723f933
Muhammad Tayyab <96627100+muhammadtayyab3411@users.noreply.github.com>
2024-08-18 23:00:19 +0500

    added passHref to NextLink

--- src/components/navigation/link.tsx
+++ src/components/navigation/link.tsx
@@ -28,6 +28,7 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
         {...props}
         target={externalLink ? '_blank' : '_self'}
         ref={ref}
+        passHref
         className={mergeClasses(
           noCustomization ??
             'text-base font-medium text-gray-600 transition-all hover:text-gray-900 active:text-gray-600',
